154cb9cab2956fd0ad55a9d077f59ece073786bf
borgesgoncalves <borgesgoncalves@outlook.com.br>
2023-09-07 18:10:47 -0300

    estalando react-router-dom e configurando paginas home, cadastro e notfound

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./paginas/home";
+import Cadastro from "./paginas/cadastro";
 
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+  <Routes>
+    <Route path="/"  element={<Home/>}/>
+    <Route path="/cadastro" element={<Cadastro/>}/>
+    <Route path="*" element={<div>Página não encontrada</div>}/>
+  </Routes>
+    </BrowserRouter>
   );
 }
 
