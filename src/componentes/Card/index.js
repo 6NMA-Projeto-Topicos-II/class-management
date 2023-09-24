a1af66e46106f43701fcdbcee58ef75861df3c38
borgesgoncalves <borgesgoncalves@outlook.com.br>
2023-09-24 12:20:07 -0300

    adicionando card de laboratorio com css definitivo e resetando css padrao

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -1,21 +1,48 @@
 import "./style.css"
 
-export default function Card (inicio, fim, titulo, lab, estado, ativo = false) {
+export default function Card (props) {
 
-    return (
-        <div className={ativo ? "composto" : "simples"}>
-            <div className="estado-num">
-                <p>{lab}</p>
-                <p>{estado}</p>
-            </div>
-            <div className="titulo">
-                <p>{titulo}</p>
-            </div>
-            <div className="horario">
-                <p>{inicio}</p>
-                <p>-</p>
-                <p>{fim}</p>
-            </div>
-        </div>
-    )
-}
+        if(`${props.estado}` === "Disponível") {
+            return(
+                <div className="card">
+                <div className="disponivel">
+                <p>{props.lab}</p>
+                <p>{props.estado}</p>
+                </div>
+                </div>
+        )} else if (`${props.estado}` === "Acesso livre") {
+            return (
+                <div className="card">
+                <div className="acesso-livre">
+                <p>{props.lab}</p>
+                <p>{props.estado}</p>
+                </div>
+                </div>
+        )} else if (`${props.estado}` === "Manuntenção") {
+            return (
+                <div className="card">
+                <div className="manuntencao">
+                <p>{props.lab}</p>
+                <p>{props.estado}</p>
+                </div>
+                </div>
+        )} else if (`${props.estado}` === "Em aula") {
+            return (
+                <div className="card">
+                    <div className="em-aula">
+                        <p>{props.lab}</p>
+                        <p>{props.estado}</p>
+                    </div>
+                    <div className="titulo">
+                        <p>{props.titulo}</p>
+                        <p className="prof">{props.professor}</p>
+                    </div>
+                    <div className="horario">
+                        <p>{props.inicio}</p>
+                        <p>-</p>
+                        <p>{props.fim}</p>
+                    </div>
+                </div>
+            )
+        }
+    }
