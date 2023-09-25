43b12b18617d629e9e67043634ea823d7b5507f5
borgesgoncalves <borgesgoncalves@outlook.com.br>
2023-09-25 19:05:30 -0300

    adding navbar e header

---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,5 @@
+import logo from "../../Assets/Logo-CH.png"
+
+export default function Logo(props) {
+    return <img className={props.className} alt="logo CampusHub" src={logo}></img>
+}
---
+++ src/paginas/configurações/index.js
@@ -0,0 +1,3 @@
+export default function Configurações() {
+    return <div></div>
+}
