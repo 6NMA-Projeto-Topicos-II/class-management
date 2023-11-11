0fcc2678153459df04d191277fe2ed02bfcd6548
borgesgoncalves <borgesgoncalves@outlook.com.br>
2023-11-11 18:48:47 -0300

    Adicionando lista de cards

--- src/paginas/cadastro/index.js
+++ src/paginas/cadastro/index.js
@@ -1,4 +1,6 @@
+import FormCadastro from './../../Componentes/FormCadastro/index';
 export default function Cadastro() {
     return <div>
+        <FormCadastro></FormCadastro>
     </div>
 }
--- src/paginas/home/index.js
+++ src/paginas/home/index.js
@@ -1,6 +1,8 @@
+import Lista from "../../Componentes/ListaCards"
 import "./style.css"
 
 export default function Home() {
     return <div className="main">
+        <Lista></Lista>
     </div>
 }
