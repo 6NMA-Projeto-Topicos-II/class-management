2135cf1929afaf6099859bc46b75bdf4300d1c8d
borgesgoncalves <borgesgoncalves@outlook.com.br>
2023-11-16 13:47:17 -0300

    adicionando pagina de login

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,18 @@ import Home from "./paginas/home";
 import Cadastro from "./paginas/cadastro";
 import NavBar from "./Componentes/Navbar";
 import Configurações from "./paginas/configurações";
+import { Login } from "./paginas/login";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar/>
+        <NavBar/>
       <Routes>
-        <Route path="/"  element={<Home/>}/>
+        <Route path="/home"  element={<Home/>}/>
         <Route path="/configurações" element={<Configurações/>}/>
-        <Route path="/cadastro" element={<Cadastro/>}/>
         <Route path="*" element={<div>Página não encontrada</div>}/>
+        <Route path="/cadastro" element={<Cadastro/>}/>
+        <Route path="/" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
   )
